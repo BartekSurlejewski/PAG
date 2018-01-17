1a523ee8cca031b99fcbf5966e95e80ed1606d0e
BartekSurlejewski <bartoszsurlejewskI@gmail.com>
2018-01-17 15:48:40 +0100

    Adding PCF

--- Core.cpp
+++ Core.cpp
@@ -7,8 +7,8 @@
 #include "PointLight.h"
 #include "SpotLight.h"
 
-#define SHADOW_WIDTH  2048
-#define SHADOW_HEIGHT  2048
+#define SHADOW_WIDTH  1920
+#define SHADOW_HEIGHT  1920
 
 
 Core* core = nullptr;
@@ -433,8 +433,11 @@ bool Core::Initialize()
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+	
+	float borderColor[] = {1.0f, 1.0f, 1.0f, 1.0f};
+	glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
 
 	//Attach depth texture as FBO's depth buffer
 	glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
@@ -495,8 +498,8 @@ void Core::Render()
 	//Render depth map
 	glm::mat4 lightProjection, lightView;
 	glm::mat4 lightSpaceMatrix;
-	float near_plane = -200.0f, far_plane = 200.0f;
-	lightProjection = glm::ortho(-200.0f, 200.0f, 200.0f, -200.0f, near_plane, far_plane);
+	float near_plane = -70.0f, far_plane = 70.0f;
+	lightProjection = glm::ortho(-70.0f, 70.0f, 70.0f, -70.0f, near_plane, far_plane);
 	lightView = glm::lookAt(directionalLightPos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	lightSpaceMatrix = lightProjection * lightView;
 	depthShader.UseProgram();
